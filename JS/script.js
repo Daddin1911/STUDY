e1469da9a8eda9464c813276c284fe40c832abd6
Dima <daddinbit@gmail.com>
2022-01-13 15:54:37 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+                                  //типи данних
+//console.log(something);
+/*
+                            undefined
+//let und;
+//console.log(und);
+                             обєкт
+//const obj = {
+//    name: 'Jonh',
+//    age: 29,
+//    isMarried: false
+//}
+//console.log(obj.name);
+//console.log(obj['name']);
+
+                             масив
+/*let arr = ['plumb.jpeg', 2, 'String'];
+console.log(arr[2]);
+*/
+
+//                                  Спілкування з користувачем розділ 2 урок 7
+
+//const result = confirm("Are you here?");
+//alert(result);
+//console.log(result);
+
+// const answer = +prompt("Вам 18?","");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers [0] = prompt('name?','');
+// answers [1] = prompt('Age?','');
+// answers [2] = prompt('18?','');
+// // // console.log(answers);
+// // document.write(answers);
+// console.log(typeof(answers));
+
+                                                        // 8 урок інтерполяція, конкатинація строк ``````
+
+// const category = 'toys';
+// // console.log('https:/someurl.com/'+ category + '/конкатинація'+ '4');
+// // console.log(`https:/someurl.com/${category}/4/інтерполяція`);
+
+// const user = 'Dmytro';
+// alert(`Hello, ${user}`); 
+
+                                                        //  9 урок, частина 2, ОПЕРАТОРИ
+
+// console.log(4 + +'5');      +'5' УНАРНИЙ ПЛЮС         
+
+// іНКРЕМЕНТ ТА ДЕКРЕМЕНТ           
+
+// let incr = 10;
+// let decr = 10;
+
+// // постфіксна форма запису
+// // incr++;
+// // decr--;
+
+// // префіксна форма запису
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5%2); оператор залишку
+
+// const isChecked = true,
+//       isClose = true;
+
+//       console.log(isChecked && isClose);
+     
+// const isChecked = true,
+//       isClose = false;
+
+//       console.log(isChecked || isClose);
+
+// таблиця ОПЕРАТОРІВ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+
